fadf662604924fdb592f33369b6eec247de81d12
Mik1810 <michaelpiccirilli3@gmail.com>
2025-04-23 23:03:00 +0200

    remade basis distribution of code

---
+++ include/nxpp.hpp
@@ -0,0 +1,10 @@
+#ifndef NXPP_HPP
+#define NXPP_HPP
+
+// Include tutti gli header del package
+#include "SimpleGraph.hpp"
+#include "SimpleDirectedGraph.hpp"
+#include "SimpleWeightedDirectedGraph.hpp"
+#include "SimpleWeightedUndirectedGraph.hpp"
+
+#endif // NXPP_HPP
--- main.cpp
+++ main.cpp
@@ -1,67 +1,130 @@
-#include "SimpleGraph.h"
+#include <iostream>
+#include <vector>
+#include "include/nxpp.hpp"
 
+int simple_undirected_graph() {
+    // Istanzio un grafo non diretto e non pesato
+    SimpleGraph g;
+    g.add_edges_from({{0, 1}, {1, 2}, {2, 3}, {3, 0}});
+
+    // Stampo i nodi e gli archi del grafo
+    std::cout << "Nodi del grafo: ";
+    for (auto node : g.nodes()) {
+        std::cout << node << " " << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "Archi del grafo: ";
+    for (auto edge : g.edges()) {
+        std::cout << "(" << edge.first << ", " << edge.second << ") " << std::endl;
+    }
+
+    return 0;
+}
+
+int simple_directed_graph() {
+    // Istanzio un grafo diretto
+    SimpleDirectedGraph g;
+    g.add_edges_from({
+        {0, 1}, 
+        {1, 2}, 
+        {2, 3}, 
+        {1, 3}
+    });
+
+    // Stampo i nodi e gli archi del grafo
+    std::cout << "Nodi del grafo: ";
+    for (auto node : g.nodes()) {
+        std::cout << node << " " << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "Archi del grafo: ";
+    for (auto edge : g.edges()) {
+        std::cout << "(" << edge.first << ", " << edge.second << ") " << std::endl;
+    }
+
+    // Eseguo un ordinamento topologico
+    std::cout << "Nodi ordinati: ";
+    for (auto node : g.topological_sort()) {
+        std::cout << node << " " << std::endl;
+    }
+
+    return 0;
+}
+
+int simple_weighted_directed_graph() {
+    // Istanzio un grafo diretto e pesato
+    SimpleWeightedDirectedGraph g;
+    g.add_edges_from({
+        {0, 1, 5}, 
+        {1, 2, 10}, 
+        {2, 3, 15}, 
+        {1, 3, 20}
+    });
+
+    // Stampo i nodi e gli archi del grafo
+    std::cout << "Nodi del grafo: ";
+    for (auto node : g.nodes()) {
+        std::cout << node << " " << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "Archi del grafo: ";
+    for (auto edge : g.edges()) {
+        std::cout << "(" << edge.first << ", " << edge.second << ") con peso " << edge.weight << std::endl;
+    }
+
+    // Eseguo un ordinamento topologico
+    std::cout << "Nodi ordinati: ";
+    for (auto node : g.topological_sort()) {
+        std::cout << node << " " << std::endl;
+    }
+
+    return 0;
+}
+
+int simple_weighted_undirected_graph() {
+    // Istanzio un grafo non diretto e pesato
+    SimpleWeightedUndirectedGraph g;
+    g.add_edges_from({{0, 1, 5}, {1, 2, 10}, {2, 3, 15}, {3, 0, 20}});
+
+    // Stampo i nodi e gli archi del grafo
+    std::cout << "Nodi del grafo: ";
+    for (auto node : g.nodes()) {
+        std::cout << node << " " << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "Archi del grafo: ";
+    for (auto edge : g.edges()) {
+        std::cout << "(" << edge.first << ", " << edge.second << ") con peso " << edge.weight << std::endl;
+    }
+
+    return 0;
+}
 
 int main() {
 
+    // Esempio di utilizzo della classe SimpleUndirectedGraph
+    std::cout << "Esempio di grafo non diretto e non pesato:" << std::endl;
+    simple_undirected_graph();
 
-    SimpleGraph G;    // Create graph
+    // Esempio di utilizzo della classe SimpleDirectedGraph
+    std::cout << "Esempio di grafo diretto:" << std::endl;
+    simple_directed_graph();
 
-    // Add nodes to the graph
-    //G.add_nodes(10);
+    // Esempio di utilizzo della classe SimpleWeightedDirectedGraph
+    std::cout << "Esempio di grafo diretto e pesato:" << std::endl;
+    simple_weighted_directed_graph();
 
-    G.add_nodes_from({0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
-    G.add_edges_from(
-        {
-            {1, 2},
-            {1, 3},
-            {1, 4},
-            {2, 5},
-            {2, 6},
-            {3, 6},
-            {4, 7},
-            {5, 8},
-            {6, 9},
-            {6, 4}
-        }
-    );
-
-    // auto a = G.add_node();
-    // auto b = G.add_node();
-    // auto c = G.add_node();
-    // auto d = G.add_node();
-    // auto e = G.add_node();
-    // auto f = G.add_node();
-    // auto g = G.add_node();
-    // auto h = G.add_node();
-
-    // Make a map that maps each integer to respective alphabet letter
-    // std::map<int, char> intToAlphabet;
-    // char letter = 'a';
-    // for (int i = 0; i < 26; ++i) {
-    //     intToAlphabet[i] = letter++;
-    // }
-
-
-    // G.add_edge(a, b);
-    // G.add_edge(a, c);
-    // G.add_edge(c, d);
-    // G.add_edge(c, e);
-    // G.add_edge(b, f);
-    // G.add_edge(f, g);
-    // G.add_edge(g, h);
-
-    //G.set_node_attr(a, "label", "A");
-    //G.set_node_attr(b, "label", "B");
-
-    std::cout << "Nodi:\n";
-    for (SimpleGraph::Node v : G.nodes()) {
-        std::cout << v  << "\n";
-    }
-
-    std::cout << "Archi:\n";
-    for (auto [u, v] : G.edges()) {
-        std::cout << "(" << u << ", " << v << ")\n";
-    }
+    // Esempio di utilizzo della classe SimpleWeightedUndirectedGraph
+    std::cout << "Esempio di grafo non diretto e pesato:" << std::endl;
+    simple_weighted_undirected_graph();
 
     return 0;
 }
